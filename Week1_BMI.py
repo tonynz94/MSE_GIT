5c0aa40146b30883836b66f067f2c43716a03d51
Tonynz <tonynz94@naver.com>
2026-08-07 13:09:46 +0900

    add requirments
    add main(),
    add requirements.txt

--- Week1_BMI.py
+++ Week1_BMI.py
@@ -1,19 +1,27 @@
-weight = 0
-height = 0
+def calBMI():
+    weight = 0
+    height = 0
+    
+    weight = float(input("input your weight(kg) : "))
+    height = float(input("input your height(m) : "))
+    
+    bmi = weight / (float)(height * height)
+    
+    if bmi < 18.5:
+        result = "underweight"
+    elif bmi < 23:
+        result = "Normal weight"
+    elif bmi < 25:
+        result = "Overweight"
+    else:
+        result = "Obese"
+    
+    print(f"BMI: {bmi:.2f}")
+    print(f"Result: {result}")
 
-weight = float(input("input your weight(kg) : "))
-height = float(input("input your height(m) : "))
 
-bmi = weight / (float)(height * height)
+def main():
+    calBMI()
 
-if bmi < 18.5:
-    result = "underweight"
-elif bmi < 23:
-    result = "Normal weight"
-elif bmi < 25:
-    result = "Overweight"
-else:
-    result = "Obese"
-
-print(f"BMI: {bmi:.2f}")
-print(f"Result: {result}")
+if __name__ == "__main__":
+    main()
