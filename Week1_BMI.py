d777eefbcc9eedd4de4d8daac0c258a1d5a59eb4
Tonynz <tonynz94@naver.com>
2026-08-07 12:53:50 +0900

    Create Week1_BMI.py
    week1 bmi calculator

---
+++ Week1_BMI.py
@@ -0,0 +1,19 @@
+weight = 0
+height = 0
+
+weight = float(input("input your weight(kg) : "))
+height = float(input("input your height(m) : "))
+
+bmi = weight / (float)(height * height)
+
+if bmi < 18.5:
+    result = "underweight"
+elif bmi < 23:
+    result = "Normal weight"
+elif bmi < 25:
+    result = "Overweight"
+else:
+    result = "Obese"
+
+print(f"BMI: {bmi:.2f}")
+print(f"Result: {result}")
